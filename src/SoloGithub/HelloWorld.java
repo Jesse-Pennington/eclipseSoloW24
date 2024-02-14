d0e0399f99fccdf995a914c4d6f7d19d2aefb4f2
jesse <jesse@fedora>
2024-02-14 05:50:45 -0500

    Changed console print text.

--- src/SoloGithub/HelloWorld.java
+++ src/SoloGithub/HelloWorld.java
@@ -3,7 +3,7 @@ package SoloGithub;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello github project.");
+		System.out.println("Hello github!!!!!!!!!!!.");
 
 	}
 
