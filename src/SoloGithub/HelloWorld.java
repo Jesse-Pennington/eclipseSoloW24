1019bcd7e311212c7aebba9c1a26b05fea940988
jesse <jesse@fedora>
2024-02-14 06:18:08 -0500

    Added third print statement.

--- src/SoloGithub/HelloWorld.java
+++ src/SoloGithub/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello github!!!!!!!!!!!.");
 		System.out.println("Java is the GREATEST!");
+		System.out.println("alternate");
 
 	}
 
